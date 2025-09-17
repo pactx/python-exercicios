ef6cf1c26f3e66c2bb566b1520b3964d1eb6aed5
Pact <seuemail@exemplo.com>
2025-09-16 22:39:17 -0300

    Atualizando exercícios

--- 082.py
+++ 082.py
@@ -1,3 +1,9 @@
+"""
+Exercício Python 082: Crie um programa que vai ler vários números e colocar em uma lista. 
+Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, 
+respectivamente. Ao final, mostre o conteúdo das três listas geradas.
+"""
+
 v = list()
 par = list()
 impar = list()
